453a7c8c943c5905f57f7e8e39d4acdf477e4c46
donal_dsilva <donal_dsilva@hotmail.com>
2016-12-10 12:44:39 -0500

    latest update

--- app.js
+++ app.js
@@ -1,36 +1,3 @@
-// var http = require('http'),
-//   fs = require('fs');
-
-// function render(path, contentType, fn) {
-//   fs.readFile(__dirname + '/' + path, 'utf-8', function (err, str) {
-//     fn(err, str, contentType);
-//   });
-// }
-
-// var app = http.createServer(function (req, res) {
-//   var httpHandler = function (err, str, contentType) {
-//     if (err) {
-//       res.writeHead(500, { 'Content-Type': 'text/plain' });
-//       res.end('An error has occured: ' + err.message);
-//     } else {
-//       res.writeHead(200, { 'Content-Type': contentType });
-//       res.end(str);
-//     }
-//   };
-//   if (req.url.indexOf('/scripts/') >= 0) {
-//     render(req.url.slice(1), 'application/javascript', httpHandler);
-//   } else if (req.headers['x-requested-with'] === 'XMLHttpRequest' && req.headers['x-vanillaajaxwithoutjquery-version'] === '1.0') {
-//     res.writeHead(200, { 'Content-Type': 'application/json' });
-//     res.end(JSON.stringify({ message: 'Hello World!' }));
-//   } else if(req.url.indexOf('/airportlister.html') >= 0){
-//  /* might not be needed as httpHandler call back does it
-//     res.writeHead(200, {'Content-Type': 'text/html'});*/
-//     render(req.url.slice(1), 'text/html', httpHandler);
-//   } else {
-//     render('views/index.html', 'text/html', httpHandler);
-//   }
-// });
-
 // module.exports = app;
 
 var http = require('http'),
@@ -77,25 +44,25 @@ router.post('/post/json', function(req, res) {
    function appendJSON(obj) {
 
 // Read in a JSON file
-    var JSONfile = fs.readFileSync('Countries.json', 'utf8');
+    var JSONfile = fs.readFileSync('Airports.json', 'utf8');
 
 // Parse the JSON file in order to be able to edit it 
     var JSONparsed = JSON.parse(JSONfile);
 
 // Add a new record into country array within the JSON file    
-    JSONparsed.country.push(obj);
+    JSONparsed.Airport.push(obj);
 
 // Beautify the resulting JSON file
     var JSONformated = JSON.stringify(JSONparsed, null, 4);
 
 // Write the updated JSON file back to the system 
-    fs.writeFileSync('Countries.json', JSONformated);
+    fs.writeFileSync('Airports.json', JSONformated);
 
 // Convert the updated JSON file to XML     
-    var XMLformated = js2xmlparser.parse("countries", JSON.parse(JSONformated));
+    var XMLformated = js2xmlparser.parse("AirportCodes", JSON.parse(JSONformated));
 
 // Write the resulting XML back to the system
-    fs.writeFileSync('Countries.xml', XMLformated);
+    fs.writeFileSync('AirportLister.xml', XMLformated);
 
    }
 
